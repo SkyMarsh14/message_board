ae7045cdecb13fe99dc15aae285a690cf4bd11d2
Sky Randy <soras1758@gmail.com>
2025-03-06 18:28:46 +0900

    refactored pool.js to work with server database

---
+++ config/populate.js
@@ -0,0 +1,41 @@
+#! /usr/bin/env node
+
+const { Client } = require("pg");
+
+const SQL = `
+CREATE TABLE IF NOT EXISTS messages (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(255),
+  text VARCHAR(255),
+  added_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO messages (username, text) 
+VALUES
+  ('Bryan', 'How\\'s it going?'),
+  ('Odin', 'Hello, world!'),
+  ('Damon', 'Another day, another message.');
+`;
+
+async function main() {
+  console.log("Seeding database...");
+
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  try {
+    await client.connect();
+    await client.query(SQL);
+    console.log("Seeding complete.");
+  } catch (error) {
+    console.error("Error seeding database:", error);
+  } finally {
+    await client.end();
+  }
+}
+
+main();
