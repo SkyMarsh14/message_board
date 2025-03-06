c874d0cbbf7e1b4cbb33b90f9c45b1d0d75a7799
Sky Randy <soras1758@gmail.com>
2025-03-06 10:37:04 +0900

    Refactoed message details to use psql

--- controllers/detailsController.js
+++ controllers/detailsController.js
@@ -1,7 +1,8 @@
 const asyncHandler = require("express-async-handler");
-const messages = require("./../models/messages.js");
+const query = require("./../db/queries");
 const detailsController = asyncHandler(async (req, res) => {
   const { msgId } = req.params;
+  const messages = await query.getAllMessages();
   res.render("msgDetails", { message: messages[msgId] });
 });
 module.exports = detailsController;
