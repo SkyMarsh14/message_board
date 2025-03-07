b9310bbdbd901ca83cfd1ee6fad338ba3d4293a5
Sky Randy <soras1758@gmail.com>
2025-03-07 12:34:38 +0900

    fix database name update cli command

--- db/queries.js
+++ db/queries.js
@@ -2,12 +2,12 @@ const pool = require("./pool");
 
 async function addUser(user, text, added) {
   return await pool.query(
-    "INSERT INTO users (username, text, added) VALUES ($1, $2, $3)",
+    "INSERT INTO messages (username, text, added) VALUES ($1, $2, $3)",
     [user, text, added]
   );
 }
 async function getAllMessages() {
-  const { rows } = await pool.query("SELECT * FROM users");
+  const { rows } = await pool.query("SELECT * FROM messages");
   return rows;
 }
 module.exports = { addUser, getAllMessages };
